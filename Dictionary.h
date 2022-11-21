da96f2ef09f22ce9bbdf59276b3a5fdf0d512f87
Dan Polonski <dan.polonski@campus.technion.ac.il>
2022-11-21 18:29:52 +0200

    Implemented dictionary addNode and started insert

--- Dictionary.h
+++ Dictionary.h
@@ -9,9 +9,9 @@ class Dictionary {
 public:
     Dictionary(bool is_sorted_by_key);
 
-    //StatusType addNode(KEY key, VALUE value);
-
+    StatusType insert(KEY key, VALUE value);
 private:
+
     template<class T, class S>
     class Node {
     public:
@@ -28,11 +28,56 @@ private:
     bool is_sorted_by_key;
     Node<KEY, VALUE>* root;
     int length;
+
+
+    Node<KEY, VALUE>* findNode(Node<KEY, VALUE>* current_root, KEY key_to_find = NULL, ) {
+        if (current_root == nullptr) {
+            return nullptr;
+        }
+
+        if (is_sorted_by_key) {
+            KEY temp = current_root->key;
+
+            if (temp == key_to_find) {
+                return current_root;
+            }
+
+            if (temp > key_to_find) {
+                if (current_root->left_son == nullptr) {
+                    return current_root;
+                }
+                return findNode(key_to_find, current_root->left_son);
+            }
+
+            if (current_root->right_son == nullptr) {
+                return current_root;
+            }
+            return findNode(key_to_find, current_root->right_son);
+        }
+    }
 };
 
 template<class KEY, class VALUE>
 Dictionary<KEY, VALUE>::Dictionary(bool is_sorted_by_key) : length(0), root(nullptr),
                                     is_sorted_by_key(is_sorted_by_key){
 }
+
+
+template<class KEY, class VALUE>
+StatusType Dictionary<KEY, VALUE>::insert(KEY key, VALUE value) {
+    try {
+        if (root == nullptr) {
+            root = new Node<KEY, VALUE>(key, value, nullptr);
+            return StatusType::SUCCESS;
+        }
+        else {
+            Node<KEY, VALUE>* current_node = root;
+        }
+    }
+    catch (std::bad_alloc) {
+        return StatusType::ALLOCATION_ERROR;
+    }
+}
+
 
 #endif //EX1_DICTIONARY_H
